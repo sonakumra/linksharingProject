fa90e4c344571fec1f59f293c263e3b4f9dd7875
sona <sona@tothenew.com>
2016-02-12 22:41:49 +0530

    domain classes added with test

--- test/unit/com/ttnd/linksharing/UserSpec.groovy
+++ test/unit/com/ttnd/linksharing/UserSpec.groovy
@@ -17,28 +17,40 @@ class UserSpec extends Specification {
 
     void "test username and email"() {
         setup: "user creating email"
-        User user = new User( firstName : firstName, lastName : lastName, email: email, password : password,username:username)
+        User user = new User(firstName: firstName, lastName: lastName, email: email, password: password, username: username)
 
-        println" hello"
+        println " hello"
 
         when: "check for valiation"
         Boolean result = user.validate()
 
         then: "test the result"
+
         result == valid
 
         where: "trying different values"
-        sno |username| firstName | lastName | email | password | valid
-        1 |"sona"| "abc" | "xyz" | "sona@yahoo.com" | "qwertyw" | true
-
+        sno | username | firstName | lastName | email | password | valid
+        1 | "sona" | "abc" | "xyz" | "sona@yahoo.com" | "qwertyw" | true
+        2 | " " | "abc" | "xyz" | "sona@yahoo.com" | "qwertyw" | false
+        3 | "sona" | " " | "xyz" | "sona@yahoo.com" | "qwertyw" | false
+        4 | "sona" | "abc" | " " | "sona@yahoo.com" | "qwertyw" | false
+        5 | "sona" | "abc" | "xyz" | " " | "qwertyw" | false
+        6 | "sona" | "abc" | "xyz" | "sona@yahoo.com" | " " | false
+        7 | null | "abc" | "xyz" | "sona@yahoo.com" | "qwertyw" | false
+        8 | "sona" | null | "xyz" | "sona@yahoo.com" | "qwertyw" | false
+        9 | "sona" | "abc" | "xyz" | null | "qwertyw" | false
+        10 | "sona" | "abc" | "xyz" | "abc" | "qwerty" | false
+        11 | "sona" | "abc" | "xyz" | "sona@yahoo.com" | " 1236" | false
+        12 | "sona" | "abc" | "xyz" | "sona@yahoo.com" | null | false
 
 
     }
-    def "check email should be unique"(){
+
+    def "check email should be unique"() {
         setup:
         String email = "sona@tothenew.com"
         String password = 'password'
-         User user = new User(firstName: "sona", lastName: "Kumra", email: email, password: password,username: "abc")
+        User user = new User(firstName: "sona", lastName: "Kumra", email: email, password: password, username: "abc")
 
         when:
         user.save()
@@ -47,7 +59,7 @@ class UserSpec extends Specification {
         user.count() == 1
 
         when:
-        User newUser = new User(firstName: "abc", lastName: "xyz", email: email, password: password,username:"zwy")
+        User newUser = new User(firstName: "abc", lastName: "xyz", email: email, password: password, username: "zwy")
         newUser.save()
 
         then:
